5750aabb9f3f1a3fd0000ab1daac339b4b18109c
Kevin Kuppens <kevinkuppens@gmail.com>
2021-02-25 15:28:32 +0100

    creation du memory game

--- scripts/main.js
+++ scripts/main.js
@@ -6,7 +6,7 @@ const snakeBtn = document.getElementsByTagName("li")[3];
 const container = document.getElementsByClassName("main-view")[0];
 const viewHeight = container.getBoundingClientRect().bottom - container.getBoundingClientRect().top;
 
-
+// NAVIGATION
 
 homeBtn.addEventListener("click", function(){
     mover.style.top=0+"px";
