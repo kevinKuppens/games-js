b396706609900f120a39f82832ebd2cf84f5ffd3
Kevin Kuppens <kevinkuppens@gmail.com>
2021-02-25 16:55:31 +0100

    add lock

--- scripts/main.js
+++ scripts/main.js
@@ -23,3 +23,42 @@ snakeBtn.addEventListener("click", function(){
     const viewHeight = container.getBoundingClientRect().bottom - container.getBoundingClientRect().top;
     mover.style.top=(-(viewHeight * 3))+"px";
 })
+
+
+// MEMORY GAME
+
+const mgContainer = document.getElementsByClassName("game-container")[0];
+
+//LOCK AVANT DE LANCER
+
+const mgStartLock = document.createElement("div");
+    mgContainer.appendChild(mgStartLock);
+    mgStartLock.classList.add("startLock")
+
+const mgStartLockBg = document.createElement("div");
+    mgStartLock.appendChild(mgStartLockBg);
+    mgStartLockBg.classList.add("lockBg");
+
+const mgLockText = document.createElement("p");
+    mgStartLockBg.appendChild(mgLockText);
+    mgLockText.classList.add("startText");
+    mgLockText.innerText = "Appuyez sur le bouton ci-dessous pour démarer le jeu."
+
+const mgStartBtn = document.createElement("button");
+    mgStartLockBg.appendChild(mgStartBtn);
+    mgStartBtn.innerText= "Start game";
+    mgStartBtn.classList.add("startButton");
+    
+        mgStartBtn.addEventListener("click", memoryGame);
+    
+
+ function memoryGame(){
+     
+     //remove lock and start game
+    function removeStartLock(){
+        mgStartLock.style.display ="none";
+    }
+    mgStartLock.style.opacity = 0;
+    setTimeout(removeStartLock,1000);
+
+}
